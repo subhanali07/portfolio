22a78f985959b85ae872069433f1c0194b05fd7b
subhanali07 <subhanali200823@gmail.com>
2026-07-22 22:55:59 +0500

    commit for contact jsx checking

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
     <>
       <section id="contact" className=" min-h-fit lg:min-h-screen w-auto px-6 lg:px-8 pt-4 pb-2 lg:py-8 leading-tight scroll-mt-28 ">
         <div className="relative inline-block mb-6 lg:mb-12 ">
-          <h1 className="font-hand text-6xl 2xl:text-8xl text-ink font-bold">
+          <h1 className="font-hand text-4xl sm:text-5xl md:text-6xl 2xl:text-8xl text-ink font-bold">
             Wanna talk???
           </h1>
           <img className="absolute hidden lg:flex top-28 left-220 2xl:left-390 2xl:top-50" src={girl} alt="Contact" />
@@ -36,7 +36,7 @@ const Contact = () => {
           </svg>
           <div className="font-mono mt-4 text-ink/70  2xl:text-2xl 2xl:mt-9 text-sm ml-3">always down to chat about projects,<br/>  ideas or just random stuff :)</div>
         </div>
-        <div className="w-md h-auto lg:w-200 bg-paper border-4 rounded-xl shadow-[6px_6px_0px_0px_#1a1a1a] 2xl:w-200 pb-4 ">
+        <div className="w-auto h-auto lg:w-auto bg-paper border-4 rounded-xl shadow-[6px_6px_0px_0px_#1a1a1a] 2xl:w-200 pb-4 ">
           <p className="mt-3 ml-9 mr-9 text-ink/70 text-sm lg:text-xl 2xl:text-2xl font-mono">there u go these are my mail address and some socials im chronically active on</p>
           <div>{socials.map((social) => (<a key={social.name} href={social.href} target="_blank" rel="noopener noreferrer" className="flex items-center justify-center gap-2 2xl:rounded-xl 2xl:border-[6px] my-4 mx-4 w-100 lg:w-190 h-18  2xl:shadow-[6px_6px_0px_0px_#1a1a1a] 2xl:hover:translate-x-1 2xl:hover:translate-y-1 2xl:hover:[shadow-2px_2px_0px_0px_#1a1a1a] font-mono font-bold text-lg border-[3px] border-ink rounded-lg bg-[#FF9F5A] shadow-[4px_4px_0px_0px_#1a1a1a] active:shadow-none transition-all duration-150 ease-in-out hover:translate-x-0.5 hover:translate-y-0.5 hover:shadow-[2px_2px_0px_0px_#1a1a1a]"
 >
