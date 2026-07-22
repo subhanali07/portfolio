2980716b5d6b100deea773cddb32caf8bca305f4
subhanali07 <subhanali200823@gmail.com>
2026-07-22 22:23:35 +0500

    kun faya kun

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -36,7 +36,7 @@ const Contact = () => {
           </svg>
           <div className="font-mono mt-4 text-ink/70  2xl:text-2xl 2xl:mt-9 text-sm ml-3">always down to chat about projects,<br/>  ideas or just random stuff :)</div>
         </div>
-        <div className="w-112 h-110 lg:w-200 lg:h-95 bg-paper border-4 rounded-xl shadow-[6px_6px_0px_0px_#1a1a1a] 2xl:w-200 ">
+        <div className="w-md h-auto lg:w-200 bg-paper border-4 rounded-xl shadow-[6px_6px_0px_0px_#1a1a1a] 2xl:w-200 pb-4 ">
           <p className="mt-3 ml-9 mr-9 text-ink/70 text-sm lg:text-xl 2xl:text-2xl font-mono">there u go these are my mail address and some socials im chronically active on</p>
           <div>{socials.map((social) => (<a key={social.name} href={social.href} target="_blank" rel="noopener noreferrer" className="flex items-center justify-center gap-2 2xl:rounded-xl 2xl:border-[6px] my-4 mx-4 w-100 lg:w-190 h-18  2xl:shadow-[6px_6px_0px_0px_#1a1a1a] 2xl:hover:translate-x-1 2xl:hover:translate-y-1 2xl:hover:[shadow-2px_2px_0px_0px_#1a1a1a] font-mono font-bold text-lg border-[3px] border-ink rounded-lg bg-[#FF9F5A] shadow-[4px_4px_0px_0px_#1a1a1a] active:shadow-none transition-all duration-150 ease-in-out hover:translate-x-0.5 hover:translate-y-0.5 hover:shadow-[2px_2px_0px_0px_#1a1a1a]"
 >
